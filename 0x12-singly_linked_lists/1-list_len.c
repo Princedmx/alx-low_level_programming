d8bc44cedcd8a61a05f1115e849d4c27cb0ddd4b
Princedmx <princedmx@gmail.com>
2022-10-21 07:16:21 -0700

    0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,15 +9,15 @@
 
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+	int count;
+
+	count = 0;
 
 	while(h != NULL)
 	{
 		h = h->next;
 		count++;
 	};
-	
-	printf("\n Total no. of nodes is %d", count);
 
 	return count;
-};
+}
