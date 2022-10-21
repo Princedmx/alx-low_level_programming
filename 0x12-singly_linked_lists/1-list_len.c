e427bafc30ab75f10a1db075e7484550cd1c6116
Princedmx <princedmx@gmail.com>
2022-10-21 07:11:17 -0700

    0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - Print Nuber of nodes
+ *
+ * Return: 
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while(h == NULL)
+	{
+		h = h->next;
+		count++;
+	};
+	
+	printf("\n Total no. of nodes is %d", count);
+
+	return count;
+};
